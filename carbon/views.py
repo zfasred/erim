0dacd49fef8b4cb0906b858a8a843a925b732313
zfasred <zfasred@gmail.com>
2025-09-16 12:28:58 +0300

    Add files via upload

--- carbon/views.py
+++ carbon/views.py
@@ -321,12 +321,29 @@ def api_dynamic_input(request, input_id=None):
     
     elif request.method == 'POST':
         data = json.loads(request.body)
+
+        subscope_names = {
+            '1.1': 'Sabit Yanma',
+            '1.2': 'Mobil Yanma', 
+            '1.3': 'Proses Emisyonları',
+            '1.4': 'Kaçak Emisyonlar',
+            '1.5': 'AFOLU',
+            '2.1': 'Elektrik Tüketimi',
+            '3.1': 'Satın Alınan Mal ve Hizmet Taşımacılığı',
+            '3.2': 'Satılan Mal ve Hizmet Taşımacılığı',
+            '3.3': 'Kiralanan Varlıklar',
+            '3.4': 'İşe Gidiş Geliş',
+            '3.5': 'İş Seyahatleri',
+            '4.1': 'Satın Alınan Mal ve Hizmetler',
+            '4.2': 'Sermaye Malları',
+            '4.3': 'Atık'
+        }
         
         # Alt kapsam bul veya oluştur
         subscope, _ = SubScope.objects.get_or_create(
             scope=data['scope'],
             code=data['subscope'],
-            defaults={'name': f"Alt Kapsam {data['subscope']}"}
+            defaults={'name': subscope_names.get(data['subscope'], f"Alt Kapsam {data['subscope']}")}
         )
         
         # CO2 hesaplama
