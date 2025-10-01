aa04fea55d64bfe6ca18259fd0c622b92b064f64
zfasred <zfasred@gmail.com>
2025-10-01 20:07:30 +0300

    Add files via upload

--- ekonaz/urls.py
+++ ekonaz/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
 
 # Medya dosyalarını sunmak için gereken blok BURADA, dosyanın en sonunda olmalı
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
